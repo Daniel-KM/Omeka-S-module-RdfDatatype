fa81803d4fa1437e301dde809f90780f107bc678
Daniel Berthereau <Daniel.github@Berthereau.net>
2018-09-03 00:00:00 +0200

    Factorized js check of value against the datatype.

--- asset/js/rdf-datatype.js
+++ asset/js/rdf-datatype.js
@@ -60,29 +60,37 @@ $(document).ready( function() {
     });
 
     $('input.value.xsd-date-time').on('keyup', function(e) {
-        // Use the official regex provided by the w3c.
-        // @url https://www.w3.org/TR/xmlschema11-2/#nt-dateTimeRep
-        var val = this.value.trim();
-        if (val === ''
-            || val.match(/^-?([1-9][0-9]{3,}|0[0-9]{3})-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])T(([01][0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9](\.[0-9]+)?|(24:00:00(\.0+)?))(Z|(\+|-)((0[0-9]|1[0-3]):[0-5][0-9]|14:00))?$/)
-        ) {
-            this.setCustomValidity('');
-        } else {
-            this.setCustomValidity(Omeka.jsTranslate('Please enter a valid ISO 8601 full date time, with or without time zone offset.'));
-        }
+        regexCheck(this, 'xsd:dateTime');
+    });
+    $('input.value.xsd-decimal').on('keyup', function(e) {
+        regexCheck(this, 'xsd:decimal');
     });
 
-    $('input.value.xsd-decimal').on('keyup', function(e) {
-        // Use a quick js check (for any number) and the official regex provided by the w3c.
-        // @url https://www.w3.org/TR/xmlschema11-2/#decimal
-        var val = this.value.trim();
-        if (val === ''
-            || ($.isNumeric(val) && val.match(/^(\+|-)?([0-9]+(\.[0-9]*)?|\.[0-9]+)$/))
-        ) {
-            this.setCustomValidity('');
-        } else {
-            this.setCustomValidity(Omeka.jsTranslate('Please enter a valid decimal number.'));
+    /**
+     * Check user input against official regexes provided by the w3c.
+     *
+     * @param object element
+     * @param string datatype
+     */
+    function regexCheck(element, datatype)
+    {
+        var regex, message;
+        var val = element.value.trim();
+        if (datatype === 'xsd:decimal') {
+            // @url https://www.w3.org/TR/xmlschema11-2/#decimal
+            regex = /^(\+|-)?([0-9]+(\.[0-9]*)?|\.[0-9]+)$/;
+            message = 'Please enter a valid decimal number.';
+        } else if (datatype === 'xsd:dateTime') {
+            // @url https://www.w3.org/TR/xmlschema11-2/#nt-dateTimeRep
+            regex = /^-?([1-9][0-9]{3,}|0[0-9]{3})-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])T(([01][0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9](\.[0-9]+)?|(24:00:00(\.0+)?))(Z|(\+|-)((0[0-9]|1[0-3]):[0-5][0-9]|14:00))?$/;
+            message = 'Please enter a valid ISO 8601 full date time, with or without time zone offset.';
         }
-    });
+
+        if (!regex || val === '' || val.match(regex)) {
+            element.setCustomValidity('');
+        } else {
+            element.setCustomValidity(Omeka.jsTranslate(message));
+        }
+    }
 
 });
